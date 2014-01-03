791dacf046907151241611092051d7d3bb418cac
Kristján Ingi Mikaelsson <kristjanmik@gmail.com>
2014-01-03 04:35:09 +0000

    Fixed if undefined or 0 ttl is given

--- index.js
+++ index.js
@@ -1,58 +1,55 @@
-module.exports = function (options) {
-	options = options || {};
+module.exports = function(options) {
+    options = options || {};
+    options.redis = options.redis || {};
 
-	if (options.redis) {
-		var redis = require('redis').createClient(options.redis.port, options.redishost, options.redis.options);
+    var redis = require('redis').createClient(options.redis.port, options.redishost, options.redis.options);
 
-		if (options.redis.password) {
-			redis.auth(redis.options.password);
-		}
-	} else {
-		options.testMode = true;
-	}
+    if (options.redis.password) {
+        redis.auth(redis.options.password);
+    }
 
-	return function (req, res, next) {
-		if (options.testMode) {
-			res.cache = function () {
-				return this;
-			}
-			return next();
-		}
+    return function(req, res, next) {
+        if (options.testMode) {
+            res.cache = function() {
+                return this;
+            }
+            return next();
+        }
 
-		var end = res.end,
-			write = res.write,
-			cache = [],
-			key = req.originalUrl;
+        var end = res.end,
+            write = res.write,
+            cache = [],
+            key = req.originalUrl;
 
-		redis.get(key, function (error, reply) {
-			if (error) {
-				return next();
-			} else if (reply) {
-				res.set('Content-Type', 'application/json');
-				res.end(reply);
-			} else {
-				res.cache = function (timeout) {
+        redis.get(key, function(error, reply) {
+            if (error) {
+                return next();
+            } else if (reply) {
+                res.set('Content-Type', 'application/json');
+                res.end(reply);
+            } else {
+                res.cache = function(timeout) {
 
-					if (typeof timeout === 'undefined') {
-						timeout = 21600; //Default timeout is 6 hours
-					}
+                    if (!timeout) {
+                        return this;
+                    }
 
-					res.write = function (chunk, encoding) {
-						cache.push(chunk)
-						write.call(res, chunk, encoding);
-					}
+                    res.write = function(chunk, encoding) {
+                        cache.push(chunk)
+                        write.call(res, chunk, encoding);
+                    }
 
-					res.end = function (chunk, encoding) {
-						if (chunk) this.write(chunk, encoding);
+                    res.end = function(chunk, encoding) {
+                        if (chunk) this.write(chunk, encoding);
 
-						redis.setex(key, timeout, cache.join(''));
+                        redis.setex(key, timeout, cache.join(''));
 
-						end.call(res);
-					};
-					return this;
-				};
-				return next();
-			}
-		});
-	}
+                        end.call(res);
+                    };
+                    return this;
+                };
+                return next();
+            }
+        });
+    }
 }
